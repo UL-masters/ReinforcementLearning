b9de6dcdd41fa7dcbb16e2070d55153372d93281
Andreea <andreea.barsan00@gmail.com>
2026-03-03 00:00:52 +0100

    added implemented Q-value iteration algorithm (1.4)

--- DynamicProgramming.py
+++ DynamicProgramming.py
@@ -20,27 +20,50 @@ class QValueIterationAgent:
         self.Q_sa = np.zeros((n_states,n_actions))
         
     def select_action(self,s):
-        ''' Returns the greedy best action in state s ''' 
-        # TO DO: Add own code
-        a = np.random.randint(0,self.n_actions) # Replace this with correct action selection
-        return a
+        ''' Returns the greedy best action in state s '''
+        return argmax(self.Q_sa[s])
         
     def update(self,s,a,p_sas,r_sas):
         ''' Function updates Q(s,a) using p_sas and r_sas '''
-        # TO DO: Add own code
-        pass
+        q_old = self.Q_sa[s, a]
+        q_new = 0.0
+
+        for s_next in range(self.n_states):
+            if p_sas[s_next] > 0:
+                q_new += p_sas[s_next] * (
+                        r_sas[s_next] + self.gamma * np.max(self.Q_sa[s_next])
+                )
+
+        self.Q_sa[s, a] = q_new
+
+        return abs(q_old - q_new)
     
     
 def Q_value_iteration(env, gamma=1.0, threshold=0.001):
     ''' Runs Q-value iteration. Returns a converged QValueIterationAgent object '''
     
     QIagent = QValueIterationAgent(env.n_states, env.n_actions, gamma)
- 
-     # TO DO: IMPLEMENT Q-VALUE ITERATION HERE
-        
-    # Plot current Q-value estimates & print max error
-    # env.render(Q_sa=QIagent.Q_sa,plot_optimal_policy=True,step_pause=0.2)
-    # print("Q-value iteration, iteration {}, max error {}".format(i,max_error))
+
+    i = 0
+    while True:
+        max_error = 0
+
+        for s in range(env.n_states):
+            for a in range(env.n_actions):
+                p_sas, r_sas = env.model(s, a)
+                error = QIagent.update(s, a, p_sas, r_sas)
+                max_error = max(max_error, error)
+
+        env.render(Q_sa=QIagent.Q_sa,
+                   plot_optimal_policy=True,
+                   step_pause=0.2)
+
+        print("Q-value iteration, iteration {}, max error {}".format(i, max_error))
+
+        i += 1
+
+        if max_error < threshold:
+            break
  
     return QIagent
 
@@ -50,8 +73,7 @@ def experiment():
     env = StochasticWindyGridworld(initialize_model=True)
     env.render()
     QIagent = Q_value_iteration(env,gamma,threshold)
-    
-    # view optimal policy
+
     done = False
     s = env.reset()
     while not done:
@@ -60,8 +82,17 @@ def experiment():
         env.render(Q_sa=QIagent.Q_sa,plot_optimal_policy=True,step_pause=0.5)
         s = s_next
 
-    # TO DO: Compute mean reward per timestep under the optimal policy
-    # print("Mean reward per timestep under optimal policy: {}".format(mean_reward_per_timestep))
+    start_state = 3
+    V_start = np.max(QIagent.Q_sa[start_state])
+
+    goal_reward = 100
+    step_reward = -1.0
+
+    N = (V_start - goal_reward) / step_reward + 1
+
+    mean_reward_per_timestep = V_start / N
+
+    print("Mean reward per timestep under optimal policy: {}".format(mean_reward_per_timestep))
     
 if __name__ == '__main__':
     experiment()
